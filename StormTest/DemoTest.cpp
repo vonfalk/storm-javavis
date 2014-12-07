2bc3f582440ebb4a92dfa827d3f6fc996e68de88
Filip Strömbäck <fstromback@gmail.com>
2014-12-07 12:51:14 +0900

    Added some code for the upcoming demo.

---
+++ StormTest/DemoTest.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "Test/Test.h"
+#include "Storm/Function.h"
+#include "Code/Function.h"
+
+BEGIN_TEST_(DemoTest) {
+
+	Path root = Path::executable() + Path(L"../root/");
+	Engine engine(root);
+
+	Package *test = engine.package(Name(L"test.bs"));
+
+	CHECK(test->find(Name(L"bar")));
+	CHECK(test->find(Name(L"Foo")));
+
+	Function *fun = as<Function>(engine.scope()->find(Name(L"test.bs.demo"), vector<Value>()));
+	void *fn = fun->pointer();
+	if (!fn)
+		break;
+
+	Int r = code::FnCall().call<Int>(fn);
+	PLN("Function test.bs.demo() returns: " << r);
+
+} END_TEST
