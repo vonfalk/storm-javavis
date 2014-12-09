b1d4e753a3fd23947cdd04ad6133beebbf83c8f9
Filip Strömbäck <fstromback@gmail.com>
2014-12-09 13:41:44 +0900

    Added simple loc counting.

---
+++ loc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "----- Storm ----------------------------------------------"
+find Storm/ StormTest/ -name "*.cpp" -or -name "*.h" | xargs perl cloc.pl
+echo "----- Code  ----------------------------------------------"
+find Code/ CodeTest/ -name "*.cpp" -or -name "*.h" | xargs perl cloc.pl
+echo "----- Total ----------------------------------------------"
+find ./ -name "*.cpp" -or -name "*.h" | xargs perl cloc.pl
