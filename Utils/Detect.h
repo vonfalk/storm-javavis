0cfb4ef67c895b5e13af2bc9122f6dadce824670
Filip Strömbäck <fstromback@gmail.com>
2015-04-16 12:31:20 +0900

    Improved the detect header to not cause compilation errors on primitive types.

--- Utils/Detect.h
+++ Utils/Detect.h
@@ -6,17 +6,17 @@
  * Usage: CREATE_DETECTOR(attribute)
  * detect_<attribute><T>::value -> true or false.
  */
-#define CREATE_DETECTOR(X)						\
-	template <class T>							\
-	class detect_##X {							\
-		struct Fallback { int X; };				\
-		struct Derived : T, Fallback {};		\
-												\
-		template <class U, U> struct Check;		\
-												\
-		typedef char ArrayOfOne[1];				\
-		typedef char ArrayOfTwo[2];				\
-												\
+#define CREATE_DETECTOR(X)												\
+	template <class T>													\
+	class detect_##X {													\
+		struct Fallback { int X; };										\
+		struct Derived : T, Fallback {};								\
+																		\
+		template <class U, U> struct Check;								\
+																		\
+		typedef char ArrayOfOne[1];										\
+		typedef char ArrayOfTwo[2];										\
+																		\
 		template <class U>												\
 		static ArrayOfOne &func(Check<int Fallback::*, &U::X> *);		\
 		template <class U>												\
@@ -24,4 +24,29 @@
 	public:																\
 	typedef detect_##X type;											\
 	enum { value = sizeof(func<Derived>(0)) == sizeof(ArrayOfTwo) };	\
-};
+	};																	\
+	template <>															\
+	class detect_##X<int> {												\
+	public:																\
+	enum { value = false };												\
+	};																	\
+	template <>															\
+	class detect_##X<nat> {												\
+	public:																\
+	enum { value = false };												\
+	};																	\
+	template <>															\
+	class detect_##X<byte> {											\
+	public:																\
+	enum { value = false };												\
+	};																	\
+	template <>															\
+	class detect_##X<char> {											\
+	public:																\
+	enum { value = false };												\
+	};																	\
+	template <>															\
+	class detect_##X<bool> {											\
+	public:																\
+	enum { value = false };												\
+	};
