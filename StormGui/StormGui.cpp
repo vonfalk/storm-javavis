b431bc72ed7e38f1ce71bfb065b7f0935753bf78
Filip Strömbäck <fstromback@gmail.com>
2015-06-09 12:51:31 +0900

    Moved the test class in StormGui to a Test file.

--- StormGui/StormGui.cpp
+++ StormGui/StormGui.cpp
@@ -4,25 +4,4 @@
 
 namespace stormgui {
 
-	Test::Test() {}
-
-	Str *Test::test() {
-		return CREATE(Str, this, L"test");
-	}
-
-	ArrayP<Str> *Test::testArray() {
-		Auto<ArrayP<Str>> arr = CREATE(ArrayP<Str>, this);
-		arr->push(steal(CREATE(Str, this, L"A")));
-		arr->push(steal(CREATE(Str, this, L"B")));
-		return arr.ret();
-	}
-
-	FnPtr<Str *, Int> *Test::testPtr() {
-		return memberWeakPtr<Str *, Int>(engine(), this, &Test::testPtr);
-	}
-
-	Str *Test::testPtr(Int v) {
-		return CREATE(Str, this, L"Test:" + ::toS(v));
-	}
-
 }
--- StormGui/StormGui.h
+++ StormGui/StormGui.h
@@ -3,19 +3,4 @@
 
 namespace stormgui {
 
-	class Test : public Object {
-		STORM_CLASS;
-	public:
-		STORM_CTOR Test();
-
-		Str *STORM_FN test();
-
-		ArrayP<Str> *STORM_FN testArray();
-
-		FnPtr<Str *, Int> *STORM_FN testPtr();
-
-	private:
-		Str *CODECALL testPtr(Int v);
-	};
-
 }
