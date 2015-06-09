ad4d1919fdf809490e0a3055317801dda32e368d
Filip Strömbäck <fstromback@gmail.com>
2015-06-09 18:18:43 +0900

    Updated historic-loc file

--- historic-loc.sh
+++ historic-loc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-storm_regex="Storm.*/.*(\.h|\.cpp)"
-code_regex="Code.*/.*(\.h|\.cpp)"
+storm_regex="(Storm.*|Shared)/.*(\.h|\.cpp)"
+code_regex="(OS|Code).*/.*(\.h|\.cpp)"
+bs_regex="root/.*\.bs"
 
 function find_files {
     local commit=$1
@@ -15,11 +16,12 @@ function extract_hashes {
 }
 
 function list_hashes {
-    git cat-file --batch=""
+    # git cat-file --batch=""
+    git cat-file --batch | grep -v "^[0-9a-f]\{40\} blob [0-9]\+$"
 }
 
 function count_input {
-    perl cloc.pl --stdin-name="foo.cpp" --quiet --csv-delimiter=";" - | tail -n1 | cut -d";" -f3-
+    perl ../cloc.pl --stdin-name="foo.cpp" --quiet --csv-delimiter=";" - | tail -n1 | cut -d";" -f3-
 }
 
 function count {
@@ -36,25 +38,43 @@ function count {
 
 function compute_sum {
     local other=$2
-    echo $1";" | while read -d";" a; do
+    echo $1 | while read -d";" a; do
 	local b=`echo $other | cut -d";" -f1`
 	echo -n $(($a + $b))";"
 	other=`echo $other | cut -d";" -f2-`
     done
 }
 
-echo "date;code blank;code comment;code code;storm blank;storm comment;storm code;sum blank;sum comment;sum code"
-
-git log --format="format:%H %ci" $1 | while read commit date time timezone; do
-    echo -n $date $time";"
-
+function compute_line {
+    local commit=$1
+    
     code=`count $commit $code_regex | tr -d "\n"`
     echo -n $code";"
 
     storm=`count $commit $storm_regex | tr -d "\n"`
     echo -n $storm";"
 
-    compute_sum $code $storm
+    bs=`count $commit $bs_regex | tr -d "\n"`
+    echo -n $bs";"
+
+    sum=`compute_sum $code";" $storm";"`
+    compute_sum $sum $bs";"
     echo ""
+}
+
+echo "Date;Code: blank;Code: comment;Code: code;Storm: blank;Storm: comment;Storm: code;Basic Storm: blank;Basic Storm: comment;Basic Storm: code;Total: blank;Total: comment;Total: code"
+
+git log --format="format:%H %ai" $1 | while read commit date time timezone; do
+    echo -n $date $time";"
+
+    cached=`grep $commit"->" loc_cache 2>/dev/null`
+    if [[ $? -ne 0 ]]
+    then
+	cached=`compute_line $commit`
+	echo $commit"->"$cached >> loc_cache
+    else
+	cached=${cached#$commit"->"}
+    fi
+    echo $cached
 
 done
