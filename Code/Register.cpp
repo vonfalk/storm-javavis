7804480ceab130e7cf48d416379fc7dcac6285d5
Filip Strömbäck <fstromback@gmail.com>
2016-08-30 13:02:48 +0200

    Started implementing RegSet.
    Signed-off-by: Filip Strömbäck <fstromback@gmail.com>

--- Code/Register.cpp
+++ Code/Register.cpp
@@ -79,4 +79,153 @@ namespace code {
 		return Register((v & ~0xF00) | s << 8);
 	}
 
+
+	RegSet::RegSet() {}
+
+	RegSet::RegSet(Register r) {
+		put(r);
+	}
+
+	RegSet::RegSet(Array<Register> *regs) {
+		for (nat i = 0; i < regs->count(); i++)
+			put(regs->at(i));
+	}
+
+	void RegSet::deepCopy(CloneEnv *env) {
+		// No need.
+	}
+
+	void RegSet::fill() {
+		put(ptrA);
+		put(ptrB);
+		put(ptrC);
+	}
+
+	Bool RegSet::has(Register r) const {
+		return read(toIndex(r)) > 0;
+	}
+
+	void RegSet::put(Register r) {
+		write(toIndex(r), max(read(toIndex(r)), toSize(r)));
+	}
+
+	Register RegSet::get(Register r) {
+		Nat size = read(toIndex(r));
+		if (size == 0)
+			return regNone;
+		return toReg(toIndex(r), size);
+	}
+
+	void RegSet::remove(Register r) {
+		write(toIndex(r), 0);
+	}
+
+	RegSet::Iter::Iter() : owner(null), pos(0) {}
+
+	RegSet::Iter::Iter(RegSet *reg) : owner(reg), pos(0) {
+		if (owner->read(0) != 0)
+			++*this;
+	}
+
+	Bool RegSet::Iter::operator ==(Iter o) const {
+		if (atEnd() || o.atEnd())
+			return atEnd() == o.atEnd();
+		else
+			return owner == o.owner && id == o.id;
+	}
+
+	Bool RegSet::Iter::operator !=(Iter o) const {
+		return !(*this == o);
+	}
+
+	RegSet::Iter &RegSet::Iter::operator ++() {
+		while (!atEnd()) {
+			pos++;
+			if (owner->read(pos) != 0)
+				break;
+		}
+
+		return *this;
+	}
+
+	RegSet::Iter RegSet::Iter::operator ++(int) {
+		Iter c = *this;
+		++*this;
+		return c;
+	}
+
+	Bool RegSet::atEnd() const {
+		return owner == null || id >= RegSet::maxReg;
+	}
+
+	Register RegSet::Iter::operator *() const {
+		return RegSet::toReg(index);
+	}
+
+	Register RegSet::Iter::v() const {
+		return RegSet::toReg(index);
+	}
+
+	Iter RegSet::begin() const {
+		return Iter(this);
+	}
+
+	Iter RegSet::end() const {
+		return Iter();
+	}
+
+	// void RegSet::toS(StrBuf *to) const;
+
+	Nat RegSet::read(Nat id) const {
+		Word *d = &data;
+		Word z = d[id / 32];
+		return (z >> (2*(id % 32))) & 0x03;
+	}
+
+	void RegSet::write(Nat id, Nat v) {
+		Word *d = &data;
+		Word &z = d[id / 32];
+		z &= ~(Word(1) << (2 * (id % 32)));
+		z |= Word(v & 0x03) << (2 * (id % 32));
+	}
+
+	Register RegSet::toReg(Nat index, Nat size) {
+		Nat v = index & 0xFF;
+		switch (size) {
+		case 1:
+			v |= 0x000;
+			break;
+		case 2:
+			v |= 0x400;
+			break;
+		case 3:
+			v |- 0x800;
+			break;
+		default:
+			WARNING(L"Invalid size!");
+			break;
+		}
+		return Register(v);
+	}
+
+	Nat RegSet::toIndex(Register reg) {
+		Nat r = reg;
+		return reg & 0xFF;
+	}
+
+	Nat RegSet::toSize(Register reg) {
+		Nat r = reg;
+		switch (reg & 0x700) {
+		case 0x000:
+			return 2;
+		case 0x100:
+		case 0x400:
+			return 1;
+		case 0x800:
+			return 3;
+		default:
+			WARNING(L"Unknown size!");
+			return 2;
+		}
+	}
 }
--- Code/Register.h
+++ Code/Register.h
@@ -48,4 +48,108 @@ namespace code {
 
 	// Get the corresponding register with another size.
 	Register STORM_FN asSize(Register r, Size size);
+
+	/**
+	 * Set of registers. Considers registers of different sizes to be the same, but keeps track of
+	 * the largest integer sized register used. Byte-sizes are promoted to 32-bit sizes.
+	 *
+	 * ptrStack and ptrBase are not considered at all.
+	 */
+	class RegSet : public Object {
+		STORM_CLASS;
+	public:
+		// Create with no registers.
+		STORM_CTOR RegSet();
+
+		// Create with a single registers.
+		STORM_CTOR RegSet(Register r);
+
+		// Array of registers -> set.
+		STORM_CTOR RegSet(Array<Register> *regs);
+
+		// Deep copy.
+		void STORM_FN deepCopy(CloneEnv *env);
+
+		// Fill with all registers except ptrStack and ptrFrame.
+		void STORM_FN fill();
+
+		// Contains a specific register?
+		Bool STORM_FN has(Register r) const;
+
+		// Add registers.
+		void STORM_FN put(Register r);
+
+		// Get the largest register seen.
+		Register STORM_FN get(Register r) const;
+
+		// Remove register.
+		void STORM_FN remove(Register r);
+
+		// Get all registers in here.
+		Array<Register> *STORM_FN all() const;
+
+		// Iterator.
+		class Iter {
+			STORM_VALUE;
+		public:
+			Iter();
+			Iter(RegSet *reg);
+
+			Bool STORM_FN operator ==(Iter o) const;
+			Bool STORM_FN operator !=(Iter o) const;
+
+			Iter &STORM_FN operator ++();
+			Iter STORM_FN operator ++(int);
+
+			Register operator *() const;
+			Register STORM_FN v() const;
+
+		private:
+			RegSet *owner;
+			Nat id;
+
+			// At end?
+			bool atEnd() const;
+		};
+
+		Iter STORM_FN begin() const;
+		Iter STORM_FN end() const;
+
+		// ToS.
+		virtual void STORM_FN toS(StrBuf *to) const;
+
+	private:
+		enum {
+			maxReg = 256
+		};
+
+		// Store largest size of each register seen. We only care about 32 or 64 bits. Stores 2 bits
+		// per register. Therefore, we need 2x256 bits = 32 Words.
+		// TODO: Maybe this is a bit too large. We only need about 2x64 bits right now.
+		Word data;
+		Word data1;
+		Word data2;
+		Word data3;
+		Word data4;
+		Word data5;
+		Word data6;
+		Word data7;
+		Word data8;
+		Word data9;
+		Word data10;
+		Word data11;
+		Word data12;
+		Word data13;
+		Word data14;
+		Word data15;
+
+		// Get/set bits. 0 = not in set, 1 = 32 bit seen, 2 = pointer seen, 3 = 64-bit seen.
+		Nat read(Nat id) const;
+		void write(Nat id, Nat v);
+
+		// To/from index.
+		static Register toReg(Nat index, Nat size);
+		static Nat toIndex(Register reg);
+		static Nat toSize(Register reg);
+	};
 }
