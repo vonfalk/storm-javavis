986dd2a6d56045dd29c491c4d2a1850f9916ab5a
Filip Strömbäck <fstromback@gmail.com>
2017-03-07 15:59:57 +0100

    Started implementing Java grammar for syntax highlighting.
    Signed-off-by: Filip Strömbäck <fstromback@gmail.com>

---
+++ root/test/java/simple.java
@@ -0,0 +1,17 @@
+package java;
+
+import foo.bar;
+import foo.bar.*;
+
+class Simple {
+	int foo[] = {}, bar = {};
+
+	int bar(int z, ... foo) {}
+
+	Simple() {
+	}
+
+	class Bar {
+		int baz;
+	}
+}
