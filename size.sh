554888ca1bfb5576378490c8e6b845f86f246fb8
Filip Strömbäck <filip.stromback@liu.se>
2017-10-24 12:04:57 +0200

    Improved the size script.
    Signed-off-by: Filip Strömbäck <filip.stromback@liu.se>

--- size.sh
+++ size.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
-size=`find . -name "*.cpp" -or -name "*.h" | xargs ls -l | awk '{ total += \$5 }; END { print total }'`
-echo "Total size: $size bytes"
+
+total=0
+
+for i in ./*/.mymake
+do
+    dir=$(dirname $i)
+    dirname=$(basename $dir)
+    if [[ $dirname = "mps_tests" ]]
+    then
+	:
+    elif [[ $dirname = "SoundLib" ]]
+    then
+	:
+    else
+	if [[ $dirname = "Core" ]]
+	then
+	    files=$(find $dir -name "*.cpp" -or -name "*.h")
+	else
+	    files=$(find $dir -name "*.cpp" -or -name "*.h" | grep -v "./.*/Gen/")
+	fi
+
+	size=$(echo $files | xargs ls -l | awk '{ total += $5 }; END { print total/1024 }')
+	printf "%10s %7.1f kb\n" $dirname $size
+
+	total=$(printf "%s\n%s" $size $total | awk '{ total += $1 }; END { print total }')
+    fi
+done
+
+printf "Total size %7.1f kB\n" $total
 
