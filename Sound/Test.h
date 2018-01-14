2551a801a1c3b1c74ad55337aa8193c1b99fc290
Filip Strömbäck <fstromback@gmail.com>
2018-01-14 23:17:20 +0100

    Removed old test code.
    Removed the DIESEL test program, since the DIESEL backend was removed
    previously. Also removed a test function inside the Sound library.
    
    Signed-off-by: Filip Strömbäck <fstromback@gmail.com>

--- Sound/Test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Core/EnginePtr.h"
-
-namespace sound {
-
-	// Simple test function which performs playback without requiring Basic Storm.
-	void STORM_FN testMe(EnginePtr e) ON(Audio);
-
-}
