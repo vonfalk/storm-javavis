b72c18244184d9b0132699ecfd1952515ade56c4
Filip Strömbäck <fstromback@gmail.com>
2018-03-02 21:30:44 +0100

    Updated the Emacs configuration to work on Unix as well.
    Signed-off-by: Filip Strömbäck <fstromback@gmail.com>

--- emacs.el
+++ emacs.el
@@ -291,7 +291,7 @@
 (load "~/Projects/storm/Plugin/emacs.el")
 (load "~/Projects/storm/Plugin/emacs-test.el")
 (setq storm-mode-root "~/Projects/storm/root")
-(setq storm-mode-compiler "~/Projects/storm/debug/Storm.exe")
+(setq storm-mode-compiler "~/Projects/storm/debug/Storm")
 (setq storm-mode-include '(
 			   ("~/Projects/storm/External/final" . "final")
 			   ("~/Projects/storm/External/thesis" . "thesis")
