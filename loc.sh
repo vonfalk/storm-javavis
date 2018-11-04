644f3f65842ba72363d917e255d021c5e1208329
Filip Strömbäck <fstromback@gmail.com>
2018-11-04 14:01:33 +0100

    Updated 'loc.sh' to ignore the large BS file used for performance testing.

--- loc.sh
+++ loc.sh
@@ -4,11 +4,11 @@ find Compiler/ CppTypes/ Core/ Test/ -name "*.cpp" -or -name "*.h" | xargs perl 
 echo "----- OS -------------------------------------------------"
 find OS/ -name "*.cpp" -or -name "*.h" | xargs perl cloc.pl
 echo "----- Code  ----------------------------------------------"
-find Code/ CodeTest/ -name "*.cpp" -or -name "*.h" | xargs perl cloc.pl
+find Code/ -name "*.cpp" -or -name "*.h" | xargs perl cloc.pl
 echo "----- Total ----------------------------------------------"
 find ./ -name "*.cpp" -or -name "*.h" | grep -viE '^\./(Old|mps|SoundLib|Linux)/' | xargs perl cloc.pl
 
 echo "----- Basic Storm ----------------------------------------"
-find ./ -name "*.bs" | xargs perl cloc.pl --force-lang=Java
+find ./ -name "*.bs" | grep -viE '^./root/test/large/*' | xargs perl cloc.pl --force-lang=Java
 echo "----- Syntax ---------------------------------------------"
 find ./ -name "*.bnf" | xargs perl cloc.pl --force-lang=Java
