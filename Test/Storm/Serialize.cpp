c9309a53eb33fe52b5e9a099728de1331a1b30e5
Filip Strömbäck <fstromback@gmail.com>
2019-01-01 16:17:47 +0100

    Added a fair number of tests to the basic serialization.
    Signed-off-by: Filip Strömbäck <fstromback@gmail.com>

--- Test/Storm/Serialize.cpp
+++ Test/Storm/Serialize.cpp
@@ -3,6 +3,24 @@
 
 BEGIN_TEST_(Serialize, BS) {
 
-	runFn<void>(S("test.bs.testSerialize"));
+	// Simple serialization.
+	CHECK(runFn<Bool>(S("test.bs.simpleSerialization")));
 
+	// Multiple times in the same stream.
+	CHECK(runFn<Bool>(S("test.bs.multipleSerialization")));
+
+	// Shuffled output (simulating altering the order of members from when serialization was made).
+	CHECK(runFn<Bool>(S("test.bs.shuffledSerialization")));
+
+	// Primitives.
+	CHECK(runFn<Bool>(S("test.bs.primitiveSerialization")));
+
+	// Reading/writing different formal types.
+	CHECK(runFn<Bool>(S("test.bs.typeDiffSerialization")));
+
+} END_TEST
+
+
+BEGIN_TESTX(SerializePerf, BS) {
+	runFn<void>(S("test.bs.serializationPerf"));
 } END_TEST
