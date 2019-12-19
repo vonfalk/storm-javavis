8e7a407873ea857a30968eac552254d94a7d2529
Filip Strömbäck <filip.stromback@liu.se>
2019-12-19 17:13:51 +0100

    Made the majority of the Graphics interface abstract.
    Signed-off-by: Filip Strömbäck <filip.stromback@liu.se>

--- Gui/Graphics.cpp
+++ Gui/Graphics.cpp
@@ -12,74 +12,6 @@ namespace gui {
 			;
 	}
 
-	void Graphics::push() {
-		throw NotSupported(L"Graphics::push()");
-	}
-
-	void Graphics::push(Float opacity) {
-		throw NotSupported(L"Graphics::push(Float)");
-	}
-
-	void Graphics::push(Rect clip) {
-		throw NotSupported(L"Graphics::push(Rect)");
-	}
-
-	void Graphics::push(Rect clip, Float opacity) {
-		throw NotSupported(L"Graphics::push(Rect, Float)");
-	}
-
-	Bool Graphics::pop() {
-		throw NotSupported(L"Graphics::pop()");
-	}
-
-	void Graphics::transform(Transform *tfm) {
-		throw NotSupported(L"Graphics::transform(Transform)");
-	}
-
-	void Graphics::lineWidth(Float w) {
-		throw NotSupported(L"Graphics::lineWidth(Float)");
-	}
-
-	void Graphics::line(Point from, Point to, Brush *brush) {
-		throw NotSupported(L"Graphics::line(Point, Point, Brush)");
-	}
-
-	void Graphics::draw(Rect rect, Brush *brush) {
-		throw NotSupported(L"Graphics::draw(Rect, Brush)");
-	}
-
-	void Graphics::draw(Rect rect, Size edges, Brush *brush) {
-		throw NotSupported(L"Graphics::draw(Rect, Size, Brush)");
-	}
-
-	void Graphics::oval(Rect rect, Brush *brush) {
-		throw NotSupported(L"Graphics::oval(Rect, Brush)");
-	}
-
-	void Graphics::draw(Path *path, Brush *brush) {
-		throw NotSupported(L"Graphics::draw(Path, Brush)");
-	}
-
-	void Graphics::fill(Rect rect, Brush *brush) {
-		throw NotSupported(L"Graphics::fill(Rect, Brush)");
-	}
-
-	void Graphics::fill(Rect rect, Size edges, Brush *brush) {
-		throw NotSupported(L"Graphics::fill(Rect, Size, Brush)");
-	}
-
-	void Graphics::fill(Brush *brush) {
-		throw NotSupported(L"Graphics::fill(Brush)");
-	}
-
-	void Graphics::fill(Path *path, Brush *brush) {
-		throw NotSupported(L"Graphics::fill(Path, Brush)");
-	}
-
-	void Graphics::fillOval(Rect rect, Brush *brush) {
-		throw NotSupported(L"Graphics::fillOval(Rect, Brush)");
-	}
-
 	void Graphics::draw(Bitmap *bitmap) {
 		draw(bitmap, Point());
 	}
@@ -96,10 +28,6 @@ namespace gui {
 		draw(bitmap, rect, 1);
 	}
 
-	void Graphics::draw(Bitmap *bitmap, Rect rect, Float opacity) {
-		throw NotSupported(L"Graphics::draw(Bitmap, ...)");
-	}
-
 	void Graphics::draw(Bitmap *bitmap, Rect src, Point topLeft) {
 		draw(bitmap, src, Rect(topLeft, topLeft + src.size()));
 	}
@@ -110,18 +38,6 @@ namespace gui {
 
 	void Graphics::draw(Bitmap *bitmap, Rect src, Rect dest) {
 		draw(bitmap, src, dest, 1);
-	}
-
-	void Graphics::draw(Bitmap *bitmap, Rect src, Rect dest, Float opacity) {
-		throw NotSupported(L"Graphics::draw(Bitmap, ...)");
-	}
-
-	void Graphics::text(Str *text, Font *font, Brush *brush, Rect rect) {
-		throw NotSupported(L"Graphics::text(Text, ...)");
-	}
-
-	void Graphics::draw(Text *text, Brush *brush, Point origin) {
-		throw NotSupported(L"Graphics::draw(Text, ...)");
 	}
 
 }
--- Gui/Graphics.h
+++ Gui/Graphics.h
@@ -18,7 +18,7 @@ namespace gui {
 	 * implementation.
 	 */
 	class Graphics : public ObjectOn<Render> {
-		STORM_CLASS;
+		STORM_ABSTRACT_CLASS;
 	public:
 		// Create.
 		STORM_CTOR Graphics();
@@ -31,83 +31,81 @@ namespace gui {
 		virtual void STORM_FN reset();
 
 		// Push the current state on the state stack.
-		virtual void STORM_FN push();
+		virtual void STORM_FN push() ABSTRACT;
 
 		// Push the current state with a modified opacity applied to the new state.
-		virtual void STORM_FN push(Float opacity);
+		virtual void STORM_FN push(Float opacity) ABSTRACT;
 
 		// Push the current state with a clipping rectangle applied to the new state.
-		virtual void STORM_FN push(Rect clip);
+		virtual void STORM_FN push(Rect clip) ABSTRACT;
 
 		// Push current state, clipping and opacity.
-		virtual void STORM_FN push(Rect clip, Float opacity);
+		virtual void STORM_FN push(Rect clip, Float opacity) ABSTRACT;
 
 		// Pop the previous state. Returns false if nothing more to pop.
-		virtual Bool STORM_FN pop();
+		virtual Bool STORM_FN pop() ABSTRACT;
 
 		// Set the transform (in relation to the previous state).
-		virtual void STORM_ASSIGN transform(Transform *tfm);
+		virtual void STORM_ASSIGN transform(Transform *tfm) ABSTRACT;
 
 		// Set the line width (in relation to the previous state).
-		virtual void STORM_ASSIGN lineWidth(Float w);
+		virtual void STORM_ASSIGN lineWidth(Float w) ABSTRACT;
 
 		/**
 		 * Draw stuff.
 		 */
 
 		// Draw a line.
-		virtual void STORM_FN line(Point from, Point to, Brush *brush);
+		virtual void STORM_FN line(Point from, Point to, Brush *brush) ABSTRACT;
 
 		// Draw a rectangle.
-		virtual void STORM_FN draw(Rect rect, Brush *brush);
+		virtual void STORM_FN draw(Rect rect, Brush *brush) ABSTRACT;
 
 		// Draw rounded rectangle.
-		virtual void STORM_FN draw(Rect rect, Size edges, Brush *brush);
+		virtual void STORM_FN draw(Rect rect, Size edges, Brush *brush) ABSTRACT;
 
 		// Draw an oval.
-		virtual void STORM_FN oval(Rect rect, Brush *brush);
+		virtual void STORM_FN oval(Rect rect, Brush *brush) ABSTRACT;
 
 		// Draw a path.
-		virtual void STORM_FN draw(Path *path, Brush *brush);
+		virtual void STORM_FN draw(Path *path, Brush *brush) ABSTRACT;
 
 		// Fill a rectangle.
-		virtual void STORM_FN fill(Rect rect, Brush *brush);
+		virtual void STORM_FN fill(Rect rect, Brush *brush) ABSTRACT;
 
 		// Fill rounded rectangle.
-		virtual void STORM_FN fill(Rect rect, Size edges, Brush *brush);
+		virtual void STORM_FN fill(Rect rect, Size edges, Brush *brush) ABSTRACT;
 
 		// Fill the entire area.
-		virtual void STORM_FN fill(Brush *brush);
+		virtual void STORM_FN fill(Brush *brush) ABSTRACT;
 
 		// Fill a path.
-		virtual void STORM_FN fill(Path *path, Brush *brush);
+		virtual void STORM_FN fill(Path *path, Brush *brush) ABSTRACT;
 
 		// Fill an oval.
-		virtual void STORM_FN fillOval(Rect rect, Brush *brush);
+		virtual void STORM_FN fillOval(Rect rect, Brush *brush) ABSTRACT;
 
 		// Draw a bitmap.
 		void STORM_FN draw(Bitmap *bitmap);
 		void STORM_FN draw(Bitmap *bitmap, Point topLeft);
 		void STORM_FN draw(Bitmap *bitmap, Point topLeft, Float opacity);
 		void STORM_FN draw(Bitmap *bitmap, Rect rect);
-		virtual void STORM_FN draw(Bitmap *bitmap, Rect rect, Float opacity);
+		virtual void STORM_FN draw(Bitmap *bitmap, Rect rect, Float opacity) ABSTRACT;
 
 		// Draw a part of a bitmap.
 		void STORM_FN draw(Bitmap *bitmap, Rect src, Point topLeft);
 		void STORM_FN draw(Bitmap *bitmap, Rect src, Point topLeft, Float opacity);
 		void STORM_FN draw(Bitmap *bitmap, Rect src, Rect dest);
-		virtual void STORM_FN draw(Bitmap *bitmap, Rect src, Rect dest, Float opacity);
-
-		// TODO: Add support for drawing a part of a bitmap in a convenient way. Eg. tile or similarly.
+		virtual void STORM_FN draw(Bitmap *bitmap, Rect src, Rect dest, Float opacity) ABSTRACT;
 
 		// Draw text the easy way. Prefer using a Text object if possible, since they give more
 		// control over the text and yeild better performance. Attempts to fit the given text inside
 		// 'rect'. No attempt is made at clipping anything not fitting inside 'rect', so any text
 		// not fitting inside 'rect' will be drawn outside the rectangle.
-		virtual void STORM_FN text(Str *text, Font *font, Brush *brush, Rect rect);
+		virtual void STORM_FN text(Str *text, Font *font, Brush *brush, Rect rect) ABSTRACT;
 
 		// Draw pre-formatted text.
-		virtual void STORM_FN draw(Text *text, Brush *brush, Point origin);
+		virtual void STORM_FN draw(Text *text, Brush *brush, Point origin) ABSTRACT;
 	};
 
 
