87e12a279df9771f2b3ba6be03ed976b547a4bba
Filip Strömbäck <filip.stromback@liu.se>
2020-02-14 11:58:30 +0100

    Handling grayscale PNG images properly.
    Signed-off-by: Filip Strömbäck <filip.stromback@liu.se>

--- Graphics/ImageLoad.cpp
+++ Graphics/ImageLoad.cpp
@@ -57,11 +57,13 @@ namespace graphics {
 
 		Image *output = null;
 		if (ok) {
+			int typeNoAlpha = type & ~PNG_COLOR_MASK_ALPHA;
+
 			// Fix the format.
-			if (type == PNG_COLOR_TYPE_PALETTE)
+			if (typeNoAlpha == PNG_COLOR_TYPE_PALETTE)
 				png_set_palette_to_rgb(png);
-			if (type == PNG_COLOR_TYPE_GRAY && depth < 8)
-				png_set_expand_gray_1_2_4_to_8(png);
+			if (typeNoAlpha == PNG_COLOR_TYPE_GRAY)
+				png_set_gray_to_rgb(png);
 			if (png_get_valid(png, info, PNG_INFO_tRNS))
 				png_set_tRNS_to_alpha(png);
 			if (depth == 16)
