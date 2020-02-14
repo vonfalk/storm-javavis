51c510cfc90ef84203ed4596fe2dc9fb8e635990
Filip Strömbäck <filip.stromback@liu.se>
2020-02-14 17:31:11 +0100

    Started implementing rvalue references in the C++ frontend.
    Currently, they clash with regular reference parameters.
    
    Signed-off-by: Filip Strömbäck <filip.stromback@liu.se>

--- root/progvis_demo/special_functions.cpp
+++ root/progvis_demo/special_functions.cpp
@@ -1,6 +1,25 @@
 class my_class {
 public:
 	my_class(int a, int b) : a(a), b(b) {}
+	my_class(const my_class &other) : a(other.a), b(other.b) {}
+	my_class(my_class &&other) : a(other.a), b(other.b) {
+		other.a = 0;
+		other.b = 0;
+	}
+
+	my_class &operator =(const my_class &other) {
+		a = other.a;
+		b = other.b;
+		return *this;
+	}
+
+	my_class &operator =(my_class &&other) {
+		a = other.a;
+		b = other.b;
+		other.a = 0;
+		other.b = 0;
+		return *this;
+	}
 
 	int a;
 	int b;
