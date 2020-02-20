dbfcb0282e7e496a857515c5ba50f7aceee93ca4
Filip Strömbäck <filip.stromback@liu.se>
2020-02-20 17:01:09 +0100

    The semantics for C-style arrays are done.
    At least for new/delete. Visualization is not done yet, though.
    
    Signed-off-by: Filip Strömbäck <filip.stromback@liu.se>

--- root/progvis_demo/arrays.cpp
+++ root/progvis_demo/arrays.cpp
@@ -4,9 +4,27 @@ struct my_type {
 	int c;
 
 	my_type() : a(1), b(2), c(3) {}
+	~my_type() { a = 0; }
 };
 
-int main() {
+void in_array() {
+	int *x = new int[2];
+	int *end = x + 10;
+	int i = 0;
+	for (int *at = x; at < end; at++) {
+		*at = i++;
+	}
+
+	delete []x;
+}
+
+void in_complex_array() {
+	my_type *x = new my_type[3];
+
+	delete []x;
+}
+
+void in_type() {
 	my_type x;
 
 	for (int *a = &x.a; a <= &x.c; ++a) {
@@ -16,6 +34,13 @@ int main() {
 	print(&x.c - &x.a);
 	print(sizeof(my_type));
 	print(sizeof(my_type *));
+}
+
+int main() {
+	// in_array();
+	in_complex_array();
+
+	in_type();
 
 	return 0;
 }
