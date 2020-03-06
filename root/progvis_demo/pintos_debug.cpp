a236636287d53146bb83d708749244db3cc95494
Filip Strömbäck <filip.stromback@liu.se>
2020-03-06 16:54:35 +0100

    Fixed some minor bugs in progvis.
    Signed-off-by: Filip Strömbäck <filip.stromback@liu.se>

---
+++ root/progvis_demo/pintos_debug.cpp
@@ -0,0 +1,27 @@
+int main() {
+	const char *str = "sihtgubed";
+	char *stri = &str[8];
+	char *buf[9];
+	char **bufi;
+	char **bufend;
+	bufi = buf;
+	bufend = &buf[9];
+	while (bufi != bufend) {
+		*bufi = stri;
+		bufi++;
+		stri--;
+	}
+
+	// Possible bug?
+	do {
+		bufi--;
+		*(*bufi) -= 32;
+	} while (bufi != buf);
+
+	while (bufi != bufend){
+		putchar(**bufi);
+		bufi++;
+	}
+
+	return 0;
+}
