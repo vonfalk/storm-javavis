f3bdd81256c86e0e6595c07ca2a5e0433790967f
Filip Strömbäck <fstromback@gmail.com>
2020-04-27 13:12:00 +0200

    Fixed evaluation order of the parameters to printf and the actual printing.
    Signed-off-by: Filip Strömbäck <fstromback@gmail.com>

--- root/progvis_demo/printf.c
+++ root/progvis_demo/printf.c
@@ -1,7 +1,11 @@
+int fn(int x) {
+	return x;
+}
+
 int main() {
 	int v = 10;
 	const char *s = "World";
-	printf("Hello %10d!\n", v);
+	printf("Hello %10d!\n", fn(v));
 	printf("Hello %10s!\n", s);
 
 	return 0;
