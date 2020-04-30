55e7239ee16354bbe410bdd248561d7797519ce5
Oscar Falk <oscfa693@student.liu.se>
2020-04-30 11:19:52 +0200

    While-loopar, break, continue och klammerparantes-statements fungerar.

--- root/java/HelloWorld.java
+++ root/java/HelloWorld.java
@@ -1,8 +1,20 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      if (false)
-        print("Statement is true!");
-      else
-        print("Statement is false!");
+      if (false) {
+        print("Statement");
+        print("is");
+        print("true!");
+      } else {
+        print("Statement");
+        print("is");
+        print("false!");
+      }
+
+      while (true) {
+        print("Printing!");
+        print("Printing, again!");
+        break;
+        print("You should not see this!");
+      }
     }
 }
