ab5cef5a05c92d9bc33240104bcda934ed5fe4ad
Oscar Falk <oscfa693@student.liu.se>
2020-05-17 23:45:49 +0200

    Implementerat följande: numeriska operatorer, parametrar (och argument), Return-statements.

--- root/java/HelloWorld.java
+++ root/java/HelloWorld.java
@@ -1,16 +1,30 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-      boolean b;
-      b = true;
-      int i;
-      i = 20;
-      if (b) {
-        print("True!");
-        b = false;
-      }
 
-      while (b) {
-        print("You should not see this.");
-      }
+  void test() {
+    print("This you should see.");
+    return;
+    print("You should not see this.");
+  }
+
+  int test2(int x) {
+    return x + 20;
+  }
+
+  int test3() {
+    return;
+  }
+
+  void test4() {
+    return 20;
+  }
+
+    public static void main() {
+      test();
+
+      int i;
+      i = test2(20);
+
+      //test3();
+      //i = test4();
     }
 }
