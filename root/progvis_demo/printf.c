2a1d21d6d0d41afd07f5afcdc3c14206b3398a19
Filip Strömbäck <filip.stromback@liu.se>
2021-10-05 14:48:25 +0200

    WIP: Pointers to array elements in C++ works in Progvis.
    Signed-off-by: Filip Strömbäck <filip.stromback@liu.se>

--- root/progvis_demo/printf.c
+++ root/progvis_demo/printf.c
@@ -5,8 +5,10 @@ int fn(int x) {
 int main() {
 	int v = 10;
 	const char *s = "World";
+	const char *t = s + 1;
 	printf("Hello %10d!\n", fn(v));
 	printf("Hello %10s!\n", s);
+	printf("Hello %10s!\n", t);
 
 	return 0;
 }
